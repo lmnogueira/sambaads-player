bfd93cf0e006757a477e78f5f9e5259a2d37abbc
lucas nogueira <lucasnogueira@gmail.com>
2015-08-18 23:21:51 -0300

    atualizando scripts gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,30 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+ 
+var paths = {
+  scripts: ['scripts/**/*.js'],
+  images:   'images/**/*'
+};
 
-gulp.task("ci", ['build', 'test'])
+gulp.task('clean', function(cb) {
+  del(['build'], cb);
+});
+
+gulp.task("build-scripts", ['clean'], function(){
+	return gulp.src(paths.scripts)
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task("ci", ['watch','build-scripts']);
+
+gulp.task('watch', function() {
+  gulp.watch(paths.scripts, ['build-scripts']);
+  gulp.watch(paths.images, ['images']);
+});
