0cd6d41d3303208b34d1ce618929484d24d21ee5
Thauan Zatta <thauanz@gmail.com>
2015-08-24 17:23:05 +0000

    add variable ambiente

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -388,7 +388,7 @@ var ExpandedCinema = function (cw, currentIframe){
 
 		parameters.w = parameters.w ? parameters.w : "100%";
 		parameters.h = parameters.h ? parameters.h : "100%";
-		parameters.request_domain = parameters.debug == "true" ? "9bb0910a.ngrok.io" : "d366amxgkdfvcq.cloudfront.net";
+		parameters.request_domain = '/* @echo CDN_DOMAIN */';
 
 		//append the iframe player
 		var iframe_data = appendIframe(parameters);
@@ -416,7 +416,7 @@ var ExpandedCinema = function (cw, currentIframe){
 	};
 
 	var onMessageReceive = function(event){
-		if(event.origin.indexOf("cloudfront") >= 0 || event.origin.indexOf("9bb0910a.ngrok.io") >= 0){
+		if(event.origin.indexOf("cloudfront") >= 0 || event.origin.indexOf('/* @echo CDN_DOMAIN */') >= 0){
 
 			var params = event.data.split("::");
 
