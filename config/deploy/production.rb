ff2413e5c7e37334a55049ff5972174a01e70222
Thauan Zatta <thauanz@gmail.com>
2015-09-25 14:16:06 +0000

    add new server 3 to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,10 +4,15 @@ set :node_env, 'production'
 
 set :with_user, "sambaads"
 
-set :hostname_list, {smartseed1: '52.7.210.178', smartseed2: '54.86.48.111'}
+set :smartseed1, '52.7.210.178'
+set :smartseed2, '54.86.48.111'
+set :smartseed3, '54.175.17.141'
 
-server fetch(:hostname_list)[:smartseed1], user: fetch(:with_user), roles: %w{app db web}
-server fetch(:hostname_list)[:smartseed2], user: fetch(:with_user), roles: %w{app db web}
+set :hostname_list, {smartseed1: fetch(:smartseed1), smartseed2: fetch(:smartseed2)}
+
+server fetch(:smartseed1), user: fetch(:with_user), roles: %w{app db web}
+server fetch(:smartseed2), user: fetch(:with_user), roles: %w{app db web}
+# server fetch(:smartseed3), user: fetch(:with_user), roles: %w{app db web}
 
 set :nvm_type, :user
 set :nvm_node, 'v0.12.2'
