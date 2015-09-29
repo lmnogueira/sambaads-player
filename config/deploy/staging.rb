8ef6af0aa48a9ec31e202b0ba85f6d46a3d84e3d
Thauan Zatta <thauanz@gmail.com>
2015-09-29 14:48:56 +0000

    add task for config nginx for player

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -13,3 +13,7 @@ set :nvm_map_bins, %w{node npm}
 set :npm_flags, '--no-spin'
 set :npm_roles, :all
 set :npm_target_path, -> { [release_path, release_path.join('app/')] }
+
+set :nginx_sever_name, "staging-player.sambaads.com"
+set :nginx_host, "http://localhost:3000/"
+set :nginx_file_name, "staging-player.conf"
