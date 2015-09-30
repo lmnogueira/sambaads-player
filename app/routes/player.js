ea0b0bdc1ef7e2c234e032ba0a65bad0f65dddc4
Thauan Zatta <thauanz@gmail.com>
2015-09-30 15:45:40 +0000

    fix add routes legacy

--- app/routes/player.js
+++ app/routes/player.js
@@ -7,9 +7,72 @@ var router = express.Router();
 router.get('/:pid', function(req, res, next) {
 	var urlFinal = req.protocol + "://" + req.hostname + req.originalUrl;
 	request.get(nconf.get("SMARTSEED_URL") + '/iframe/' + req.params.pid + '/data?' + querystring.stringify(req.query), function(error, response, body){
-		res.render('player/iframe', { base_url: urlFinal, info: JSON.parse(body) });
+		if(response.statusCode == 200){
+			res.render('player/iframe', { base_url: urlFinal, info: JSON.parse(body) });
+		}else{
+			res.sendStatus(response.statusCode);
+		}
 	});
 });
+
+// BEGINNIG OF LEGACY ROUTES DO NOT DELETE
+router.get('/player/single_player/:media_id/:pid', function(req, res, next) {
+	var urlFinal = req.protocol + "://" + req.hostname + req.originalUrl;
+
+	req.query.id = req.params.pid;
+	req.query.m  = req.params.media_id;
+	request.get(nconf.get("SMARTSEED_URL") + '/iframe/' + req.params.pid + '/data?' + querystring.stringify(req.query), function(error, response, body){
+		if(response.statusCode == 200){
+			res.render('player/iframe', { base_url: urlFinal, info: JSON.parse(body) });
+		}else{
+			res.sendStatus(response.statusCode);
+		}
+	});
+});
+
+router.get('/player/:category/:size', function(req, res, next) {
+	var urlFinal = req.protocol + "://" + req.hostname + req.originalUrl;
+	req.query.c  = req.params.category;
+	req.query.t  = (req.query.tags || "");
+
+	request.get(nconf.get("SMARTSEED_URL") + '/iframe/' + req.query.pid + '/data?' + querystring.stringify(req.query), function(error, response, body){
+		if(response.statusCode == 200){
+			res.render('player/iframe', { base_url: urlFinal, info: JSON.parse(body) });
+		}else{
+			res.sendStatus(response.statusCode);
+		}
+	});
+});
+
+router.get('/player/:category/:size/:pid', function(req, res, next) {
+	var urlFinal = req.protocol + "://" + req.hostname + req.originalUrl;
+	req.query.c  = req.params.category;
+	req.query.t  = (req.query.tags || "");
+
+	request.get(nconf.get("SMARTSEED_URL") + '/iframe/' + req.params.pid + '/data?' + querystring.stringify(req.query), function(error, response, body){
+		if(response.statusCode == 200){
+			res.render('player/iframe', { base_url: urlFinal, info: JSON.parse(body) });
+		}else{
+			res.sendStatus(response.statusCode);
+		}
+	});
+});
+
+router.get('/player/:category/:size/:pid/:tags', function(req, res, next) {
+	var urlFinal = req.protocol + "://" + req.hostname + req.originalUrl;
+	req.query.id = req.params.pid;
+	req.query.c  = req.params.category;
+	req.query.t  = req.params.tags;
+
+	request.get(nconf.get("SMARTSEED_URL") + '/iframe/' + req.params.pid + '/data?' + querystring.stringify(req.query), function(error, response, body){
+		if(response.statusCode == 200){
+			res.render('player/iframe', { base_url: urlFinal, info: JSON.parse(body) });
+		}else{
+			res.sendStatus(response.statusCode);
+		}
+	});
+});
+// END OF LEGACY ROUTES DO NOT DELETE
 
 router.get('/teste/:type_player', function(req, res, next) {
 	res.render('player/teste', { type_player: req.params.type_player });
