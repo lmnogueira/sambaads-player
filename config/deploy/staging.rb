06185cc06ea54ab8423b8b908e33734dfc5cf23f
Thauan Zatta <thauanz@gmail.com>
2015-10-02 14:53:00 +0000

    change ip config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@ set :node_env, 'staging'
 
 set :with_user, "sambaads"
 
-server '54.152.255.43', user: fetch(:with_user), roles: %w{app db web}
+server '52.23.212.206', user: fetch(:with_user), roles: %w{app db web}
 
 set :nvm_type, :user
 set :nvm_node, 'v0.12.2'
