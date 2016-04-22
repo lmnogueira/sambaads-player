017feec372bcb712400b9cc4e31e7ea286783775
lucas nogueira <lucasnogueira@gmail.com>
2016-04-22 14:30:17 -0300

    fix time slot

--- src/scripts/sambaads.player.js
+++ src/scripts/sambaads.player.js
@@ -168,21 +168,25 @@ SambaAdsPlayerControler.prototype.watchedCount = function(position, duration){
 
 	if((position != this.time_position) && (this.old_percent != percent)){
 
-		this.time_position = position;
-		this.old_percent = percent;
-		var time_slot = (duration*percent_frequency/100);
+		this.time_position = parseInt(position);
+		this.old_percent = parseInt(percent);
+
+		duration = parseInt(duration) < 0 || isNaN(duration) ? 0 : parseInt(duration)
+
+		var time_slot = parseFloat((parseInt(duration)*parseInt(percent_frequency)/100));
+		time_slot = time_slot < 0 || isNaN(time_slot) ? 0 : time_slot;
 
 		if(this.old_percent%percent_frequency == 0){
 			this.sendGif_v2('watched',{
 				"satm_session": this.session(),
 				"satm_client_id": "",
-				"satm_time_slot": time_slot < 0 ? 0 : time_slot,
+				"satm_time_slot": time_slot,
 				"satm_tag": "video.watched." + percent,
 				"satm_site_id": this.response.publisher_info.hash_code || this.response.site_info.hash_code,
 				"satm_media_id": parseInt(this._options.playlist[this.currentPlaylistIndex].media_id),
 				"satm_channel_id": parseInt(this._options.playlist[this.currentPlaylistIndex].channel_id || this._options.playlist[this.currentPlaylistIndex].owner_id),
 				"satm_domain": "",
-				"satm_duration": parseInt(duration) < 0 ? 0 : parseInt(duration),
+				"satm_duration": duration,
 				"satm_origin": this.response.player_info.origin,
 				'satm_environment': this.getEnvironment()
 			});
