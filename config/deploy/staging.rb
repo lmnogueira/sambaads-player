305541caecc7470d419d04cf25bc71ca2f4b9918
Thauan Zatta <thauanz@gmail.com>
2016-06-20 12:12:10 +0000

    change deploy and config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,13 +8,13 @@ set :with_user, "sambaads"
 server '52.23.212.206', user: fetch(:with_user), roles: %w{app db web}
 
 set :nvm_type, :user
-set :nvm_node, 'v0.12.2'
+set :nvm_node, 'v4.4.0'
 set :nvm_map_bins, %w{node npm}
 
 set :npm_flags, '--no-spin'
 set :npm_roles, :all
 set :npm_target_path, -> { [release_path, release_path.join('app/')] }
 
-set :nginx_sever_name, "staging-player.sambaads.com"
+set :nginx_sever_name, "staging-v2-player.sambaads.com"
 set :nginx_host, "http://localhost:3002/"
 set :nginx_file_name, "player.conf"
