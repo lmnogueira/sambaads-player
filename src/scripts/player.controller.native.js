4f3de9040139d7ef3e54b479ec005f96d2996906
Guto Mezêncio <gutomezencio@gmail.com>
2016-08-24 15:35:57 -0300

    Update native controller

--- src/scripts/player.controller.native.js
+++ src/scripts/player.controller.native.js
@@ -5,54 +5,48 @@ SambaAdsPlayerControllerNative = function (){
 		displayOverlay = $('#display-overlay'),
 		showAdTimeout = null;
 
-	var videoType = {
-			'60474': 'glam_box',
-			'60475': 'glam_mag',
-			'60476': 'glam_club'
-		};
-
-	var glamboxData = {
-			glam_box: {
-				text: '<div class="simple-text"><span>Quer experimentar novos produtos todo mês e recebê-los na sua casa?</span></div>',
-				action_url: 'https://www.glambox.com.br/Landing/show/DescontoAssinaturaAgosto2016?utm_source=YContent&utm_medium=Native&utm_content=BatomLiquido',
-				highlight_text: '<span>Assine com</span><span class="large-text">R$60</span><span>de desconto</span>',
-				impression_url: '',
-				click_url: ''
-			},
-			glam_mag: {
-				text: '<div class="simple-text single-line"><span>Quer mais dicas de beleza?</span></div>',
-				action_url: 'https://www.glambox.com.br/Revista/?utm_source=YContent&utm_medium=Native&utm_content=Top5Sombra',
-				highlight_text: '<span class="single-text">Conheça o GLAM MAG</span>',
-				impression_url: '',
-				click_url: ''
-			},
-			glam_club: {
-				text: '<div class="simple-text"><span>Fique por dentro das últimas novidades de beleza!</span></div>',
-				action_url: 'https://www.glambox.com.br/Landing/show/quetalglambox?utm_source=YContent&utm_medium=Native&utm_content=TrancasBoxeador',
-				highlight_text: '<span>Faça parte desse clube exclusivo</span>',
-				impression_url: '',
-				click_url: ''
-			}
-		};
-
-
-	var glamboxRandon = {
-			styles: [
-				'type-1',
-				'type-2'
-			]
-		};
-
-	var glamboxNative = function(videoId) {
-			var glamboxTrigger = $('#glambox-trigger');
+	var glamboxNative = function(videoId, newAd) {
+			var videoType = {
+					'60474': 'glam_box',
+					'60475': 'glam_mag',
+					'60476': 'glam_club'
+				},
+				glamboxData = {
+					glam_box: {
+						text: '<div class="simple-text"><span>Quer experimentar novos produtos todo mês e recebê-los na sua casa?</span></div>',
+						action_url: 'https://www.glambox.com.br/Landing/show/DescontoAssinaturaAgosto2016?utm_source=YContent&utm_medium=Native&utm_content=BatomLiquido',
+						highlight_text: '<span>Assine com</span><span class="large-text">R$60</span><span>de desconto</span>',
+						impression_url: '',
+						click_url: ''
+					},
+					glam_mag: {
+						text: '<div class="simple-text single-line"><span>Quer mais dicas de beleza?</span></div>',
+						action_url: 'https://www.glambox.com.br/Revista/?utm_source=YContent&utm_medium=Native&utm_content=Top5Sombra',
+						highlight_text: '<span class="single-text">Conheça o GLAM MAG</span>',
+						impression_url: '',
+						click_url: ''
+					},
+					glam_club: {
+						text: '<div class="simple-text"><span>Fique por dentro das últimas novidades de beleza!</span></div>',
+						action_url: 'https://www.glambox.com.br/Landing/show/quetalglambox?utm_source=YContent&utm_medium=Native&utm_content=TrancasBoxeador',
+						highlight_text: '<span>Faça parte desse clube exclusivo</span>',
+						impression_url: '',
+						click_url: ''
+					}
+				},
+				glamboxRandon = {
+					styles: [
+						'type-1',
+						'type-2'
+					]
+				},
+				glamboxTrigger = $('#glambox-trigger');
 
 			self.currentData = {
 					id: videoType[videoId],
 					typeData: glamboxData[videoType[videoId]],
 					style: glamboxRandon.styles[Math.floor(Math.random() * glamboxRandon.styles.length)]
 				};
-
-			self.loadVastTag();
 
 			var triggerContent = glamboxTrigger[0].innerHTML;
 			triggerContent = triggerContent.replace('{{glamText}}', self.currentData.typeData.text);
@@ -62,20 +56,31 @@ SambaAdsPlayerControllerNative = function (){
 
 			glamboxTrigger.addClass(self.currentData.style);
 
-			glamboxTrigger.on('click', function(event){
-				event.preventDefault();
-				console.log(self.currentData.typeData.click_url);
-				window.open(self.currentData.typeData.click_url);
+			self.loadVastTag(function(vastData, data){
+				glamboxTrigger.on('click', function(event){
+					event.preventDefault();
+					window.open(vastData.click_url);
+				});
 			});
-
 		};
 
-	self.startNative = function(e){
-		showAdTimeout = setTimeout(function(){
-							displayOverlay.addClass('active-native');
-							self.trackImpression();
-						}, 7000);
+	var glamboxNativeUpdate = function(videoId) {
+			glamboxTrigger = $('#glambox-new-trigger');
 
+			glamboxTrigger.on('click', function(event){
+				event.preventDefault();
+				window.open(self.currentData.typeData.click_url);
+			});
+		};
+
+	self.setAdTimeout = function(time) {
+		showAdTimeout = setTimeout(function(){
+			displayOverlay.addClass('active-native');
+			self.trackImpression();
+		}, time);
+	};
+
+	self.startNative = function(e){
 		self.video = e.detail.data;
 
 		var ownerId = e.detail.data.owner_id,
@@ -86,9 +91,11 @@ SambaAdsPlayerControllerNative = function (){
 		videoId = 60474;
 
 		if(ownerId === 38) {
-			hashCode = 'glambox';
-			//hashCode = 'glambox-new';
-			glamboxNative(videoId.toString());
+			self.setAdTimeout(7000);
+			//hashCode = 'glambox';
+			//glamboxNative(videoId.toString());
+			hashCode = 'glambox-new';
+			glamboxNativeUpdate(videoId.toString());
 		}
 
 		if(hashCode !== null) {
@@ -109,37 +116,44 @@ SambaAdsPlayerControllerNative = function (){
 		$.get( url );
 	};
 
-	self.loadVastTag = function(){
-
-		var tags = self.video.LR_TAGS + ",native," + self.currentData.id + "," + self.currentData.style;
-		var custom_params = encodeURIComponent("duration=&CNT_Position=preroll&category=" + self.video.LR_VERTICALS + "&CNT_PlayerType=singleplayer&CNT_MetaTags=" + tags);
+	self.loadVastTag = function(callback){
+		var tags = self.video.LR_TAGS + ",native," + self.currentData.id + "," + self.currentData.style,
+			custom_params = encodeURIComponent("duration=&CNT_Position=preroll&category=" + self.video.LR_VERTICALS + "&CNT_PlayerType=singleplayer&CNT_MetaTags=" + tags);
 
  		var tagUrl = "https://pubads.g.doubleclick.net/gampad/ads?" +
- 		"sz=640x360" +
- 		"&iu=" + "/387067271/Homologacao/TesteNative" + //self.client.ad_unit_id +
- 		"&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=&description_url=" +
- 		"&cust_params=" + custom_params +
- 		"&cmsid=" + self.video.dfp_partner_id +
- 		"&vid=" + self.video.hashed_code +
- 		"&correlator=" + new Date().getTime();
-
+			 		 "sz=640x360" +
+			 		 "&iu=" + "/387067271/Homologacao/TesteNative" + //self.client.ad_unit_id +
+			 		 "&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&url=&description_url=" +
+			 		 "&cust_params=" + custom_params +
+			 		 "&cmsid=" + self.video.dfp_partner_id +
+			 		 "&vid=" + self.video.hashed_code +
+			 		 "&correlator=" + new Date().getTime();
 
  		console.log(tagUrl);
 
 		$.ajax({
-        type: "get",
-        url:  tagUrl,
-        dataType: "xml",
-        success: function(data) {
-            var el = data.getElementsByTagName("Impression")[0].childNodes[0];
-            self.currentData.typeData.impression_url = el.nodeValue;
+	        type: "get",
+	        url:  tagUrl,
+	        dataType: "xml",
+	        success: function(data) {
+				var vastData = {
+						impression_url: '',
+						click_url: ''
+					};
 
-            el = data.getElementsByTagName("ClickThrough")[0].childNodes[0];
-            self.currentData.typeData.click_url = el.nodeValue;
-        },
-        error: function(xhr, status) {
-            console.log("error");
-        }
+	            var el = data.getElementsByTagName("Impression")[0].childNodes[0];
+	            vastData.impression_url = el.nodeValue;
+
+	            el = data.getElementsByTagName("ClickThrough")[0].childNodes[0];
+	            vastData.click_url = el.nodeValue;
+
+				if(typeof callback === 'function') {
+					callback(vastData, data);
+				}
+	        },
+	        error: function(xhr, status) {
+	            console.log("error");
+	        }
     	});
 	}
 
