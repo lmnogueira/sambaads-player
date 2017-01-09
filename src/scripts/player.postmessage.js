49a8cf0a3fc8e60b75c7140cff5d0ade741795f8
Lucas Marques Nogueira <lucasnogueira@gmail.com>
2017-01-09 13:38:31 -0200

    fix player

--- src/scripts/player.postmessage.js
+++ src/scripts/player.postmessage.js
@@ -66,9 +66,10 @@ SambaAdsPlayerPostMessage.prototype.onPause = function(data){
 };
 
 SambaAdsPlayerPostMessage.prototype.onReady = function(iframeID, data){
-		window.sambaads.parentIframeID = iframeID; //iframe id received from parent
-		window.sambaads.pageDescription = atob(data);
-		this.sendMessage("onReady", this.configuration.client.auto_start + "," + this.configuration.player.width + "," + this.configuration.player.height );	
+	window.sambaads.parentIframeID = iframeID; //iframe id received from parent
+	window.sambaads.pageDescription = atob(data);
+	this.sendMessage("onReady", this.configuration.client.auto_start + "," + this.configuration.player.width + "," + this.configuration.player.height );
+
 	//this.sendMessage("onNowWatchTitle", this.getCurrentVideo().title);
 };
 
