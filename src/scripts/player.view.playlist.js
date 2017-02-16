3dda413c57085d4e9d28c4086743fc80e9507bcd
Lucas Marques Nogueira <lucasnogueira@gmail.com>
2017-02-16 16:41:58 -0200

    change frequency native display

--- src/scripts/player.view.playlist.js
+++ src/scripts/player.view.playlist.js
@@ -82,8 +82,11 @@ SambaAdsPlayerViewPlaylist.prototype.init = function(options){
 		}
 	}
 
+	var peso = [0,2,4,6,1]
+	var index = peso[Math.floor(Math.random() * 5)]%2
+
 	if(self.playlist[i] && self.playlist[i].sponsored){
-	  	self.playlist.move(i,(Math.floor(Math.random() * 2) ));
+	  	self.playlist.move(i,index);
 	}
 
 	self.playlist.forEach(function(item){
