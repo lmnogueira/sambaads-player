fd02762b29d36adbb18a0831fa22db66cc7fabb1
Lucas Nogueira <lucasnogueira@gmail.com>
2017-06-07 22:45:43 -0300

    fix wrong variable

--- src/scripts/player.controller.collector.js
+++ src/scripts/player.controller.collector.js
@@ -120,7 +120,7 @@ SambaAdsPlayerControllerCollector = function (){
 	SambaAdsPlayerMessageBroker().addEventListener(Event.PLAY_LIST_ITEM, function(e){
 		self.media = e.detail.data;
 
-		if([117514,117515,114885].indexOf(parseInt(self.media.id)) > -1){
+		if([117514,117515,114885].indexOf(parseInt(self.media.media_id)) > -1){
 			self.trackPixel("https://ad.doubleclick.net/ddm/trackimp/N416407.2485700Y-CONTENT/B11125712.152853381;dc_trk_aid=322939942;dc_trk_cid=80850009;ord=" + Date.now() + ";dc_lat=;dc_rdid=;tag_for_child_directed_treatment=?" );
 		}
 	});
